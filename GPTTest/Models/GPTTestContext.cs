7913955d72028dd50e44af9a985f1907ab625d5f
David Murray <dmurray@clarendontech.com>
2023-06-24 00:30:41 -0400

    init commit

---
+++ GPTTest/Models/GPTTestContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace GPTTest.Models;
+
+public class GptTestContext : DbContext
+{
+    public GptTestContext(DbContextOptions<GptTestContext> options)
+    :base (options)
+    {
+        
+    }
+
+    public DbSet<Appointment> Appointments { get; set; } = null!;
+
+}
